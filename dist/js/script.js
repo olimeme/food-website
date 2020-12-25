b79251cf11a3da32e4d7232c30f2273fd3796825
Alibek <alibek.mombekov@mail.ru>
2020-12-25 16:14:20 +0600

    timer added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+    //tabs
     const tabs = document.querySelectorAll('.tabheader__item');
     const tabcontent = document.querySelectorAll('.tabcontent');
     const tabsParent = document.querySelector('.tabheader__items');
@@ -42,4 +43,52 @@ document.addEventListener('DOMContentLoaded', ()=>{
             });
         }
     });
+    //tabs
+    //timer
+    const deadline = '2021-01-30';
+    function getTimeRemaining(endtime)
+    {
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        const days = Math.floor(t/(1000*60*60*24));
+        const hours = Math.floor((t/(1000*60*60)) % 24);
+        const minutes = Math.floor((t/(1000*60)) % 60);
+        const seconds = Math.floor((t/1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(parentBlock,endtime){
+        const timer = document.querySelector(parentBlock);
+        const days = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        const timeInterval = setInterval(updateClock,1000);
+        
+        updateClock();
+        
+        function updateClock(){
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZeroPlace(t.days);
+            hours.innerHTML = getZeroPlace(t.hours);
+            minutes.innerHTML = getZeroPlace(t.minutes);
+            seconds.innerHTML = getZeroPlace(t.seconds);
+
+            if(t.total <= 0)
+                clearInterval(timeInterval);
+        }
+    }
+
+    function getZeroPlace(num)
+    {   
+        return (num >= 0 && num < 10) ? `${0}${num}` : `${num}`
+    }
+    setClock('.timer',deadline);
+    //timer
 });
